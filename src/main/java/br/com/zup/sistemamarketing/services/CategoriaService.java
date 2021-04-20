ed695237010d8d1d5893787af80f61f4bfed96a8
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-20 15:36:08 -0300

    Criar método para cadastrar um novo produto

--- src/main/java/br/com/zup/sistemamarketing/services/CategoriaService.java
+++ src/main/java/br/com/zup/sistemamarketing/services/CategoriaService.java
@@ -76,5 +76,5 @@ public class CategoriaService {
         }
 
         return optionalCategoria.get();
-    }   
+    }
 }
--- src/main/java/br/com/zup/sistemamarketing/services/ProdutoService.java
+++ src/main/java/br/com/zup/sistemamarketing/services/ProdutoService.java
@@ -1,8 +1,13 @@
 package br.com.zup.sistemamarketing.services;
 
+import br.com.zup.sistemamarketing.models.Categoria;
+import br.com.zup.sistemamarketing.models.Produto;
 import br.com.zup.sistemamarketing.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProdutoService {
@@ -14,5 +19,18 @@ public class ProdutoService {
     public ProdutoService(ProdutoRepository produtoRepository, CategoriaService categoriaService) {
         this.produtoRepository = produtoRepository;
         this.categoriaService = categoriaService;
+    }
+
+    public Produto cadastrarNovoProduto(Produto produto) {
+        produto.setCategorias(gerarListaCategoriaParaCadastro(produto.getCategorias()));
+        return produtoRepository.save(produto);
+    }
+
+    private List<Categoria> gerarListaCategoriaParaCadastro(List<Categoria> categorias) {
+        List<Categoria> categoriasAdcionarNoProduto = new ArrayList<>();
+        for (Categoria categoria : categorias) {
+            categoriasAdcionarNoProduto.add(categoriaService.verificarCategoriaPorNome(categoria));
+        }
+        return categoriasAdcionarNoProduto;
     }
 }
