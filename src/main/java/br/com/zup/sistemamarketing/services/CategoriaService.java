221d134c81c7df96fc9eacc4d765e3d0f14bb7a4
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-20 14:04:23 -0300

    Criar serviço de categoria

--- src/main/java/br/com/zup/sistemamarketing/services/CategoriaService.java
+++ src/main/java/br/com/zup/sistemamarketing/services/CategoriaService.java
@@ -1,2 +1,16 @@
-package br.com.zup.sistemamarketing.services;public class CategoriaService {
+package br.com.zup.sistemamarketing.services;
+
+import br.com.zup.sistemamarketing.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    @Autowired
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 }
