9fd5d696234f97d3e962d532f7ba203015162127
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-20 14:01:34 -0300

    Criar entidade de categoria

---
+++ src/main/java/br/com/zup/sistemamarketing/models/Categoria.java
@@ -0,0 +1,45 @@
+package br.com.zup.sistemamarketing.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 80)
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
