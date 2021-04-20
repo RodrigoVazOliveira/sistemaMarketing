2201e5aed6d4dba6e8b83516c210fbd09ee4e882
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-20 14:23:19 -0300

    Criar DTO para cadastrar categoria

---
+++ src/main/java/br/com/zup/sistemamarketing/dtos/categoria/entrada/CadastrarCategoriaDTO.java
@@ -0,0 +1,29 @@
+package br.com.zup.sistemamarketing.dtos.categoria.entrada;
+
+import br.com.zup.sistemamarketing.models.Categoria;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class CadastrarCategoriaDTO {
+
+    @NotNull(message = "O campo nome não foi informado")
+    @NotEmpty(message = "O campo nome deve ser preenchido!")
+    @Length(message = "O campo nome possui um limite de 80 caracteres")
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria converterDtoParaCategoria() {
+        Categoria categoria = new Categoria();
+        categoria.setNome(this.nome);
+        return categoria;
+    }
+}
