47fd9c139c1ccf37924d719ee8e2427264ab1c25
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-20 14:27:28 -0300

    Criar método de POST para cadastrar nova categoria

--- src/main/java/br/com/zup/sistemamarketing/controllers/CategoriaController.java
+++ src/main/java/br/com/zup/sistemamarketing/controllers/CategoriaController.java
@@ -1,9 +1,14 @@
 package br.com.zup.sistemamarketing.controllers;
 
+import br.com.zup.sistemamarketing.dtos.categoria.entrada.CadastrarCategoriaDTO;
+import br.com.zup.sistemamarketing.dtos.categoria.saida.SaidaCategoriaDTO;
+import br.com.zup.sistemamarketing.models.Categoria;
 import br.com.zup.sistemamarketing.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("categorias/")
@@ -11,8 +16,17 @@ public class CategoriaController {
 
     private final CategoriaService categoriaService;
 
-    @Autowired  
+    @Autowired
     public CategoriaController(CategoriaService categoriaService) {
         this.categoriaService = categoriaService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public SaidaCategoriaDTO cadastrarNovaCategoria(@RequestBody @Valid
+                                                            CadastrarCategoriaDTO cadastrarCategoriaDTO) {
+        Categoria categoria = categoriaService.cadastrarNovaCategoria(
+                cadastrarCategoriaDTO.converterDtoParaCategoria());
+        return SaidaCategoriaDTO.converterModeloParaDto(categoria);
     }
 }
