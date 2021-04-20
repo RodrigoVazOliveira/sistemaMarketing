a087f522cc107d897e9613bebdaac582598713c0
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-20 15:47:07 -0300

    Criar controller de produto

---
+++ src/main/java/br/com/zup/sistemamarketing/controllers/ProdutoController.java
@@ -0,0 +1,20 @@
+package br.com.zup.sistemamarketing.controllers;
+
+import br.com.zup.sistemamarketing.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos/")
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @Autowired
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
+    
+}
