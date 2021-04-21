74dc0f3d62a70d97450e5f1c08319b7bb4c976a1
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-21 08:41:41 -0300

    Criar DTO de contatoProdutoCategoriaDTO para cadastro de contato

---
+++ src/main/java/br/com/zup/sistemamarketing/dtos/contato/entrada/CadastrarContatoProdutoCategoriaDTO.java
@@ -0,0 +1,39 @@
+package br.com.zup.sistemamarketing.dtos.contato.entrada;
+
+import br.com.zup.sistemamarketing.models.Categoria;
+
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastrarContatoProdutoCategoriaDTO {
+
+    @NotNull(message = "O campo categorias não foi informado")
+    private List<String> categorias;
+
+    public CadastrarContatoProdutoCategoriaDTO() {
+    }
+
+    public CadastrarContatoProdutoCategoriaDTO(List<String> categorias) {
+        this.categorias = categorias;
+    }
+
+    public List<String> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<String> categorias) {
+        this.categorias = categorias;
+    }
+
+    public List<Categoria> converterDtoParaModelo() {
+        List<Categoria> categoriasModelos = new ArrayList<>();
+        for (String nome : categorias) {
+            categoriasModelos.add(
+                    new Categoria(nome)
+            );
+        }
+
+        return categoriasModelos;
+    }
+}
---
+++ src/main/java/br/com/zup/sistemamarketing/dtos/contato/entrada/CadastrarContatoProdutoDTO.java
@@ -0,0 +1,4 @@
+package br.com.zup.sistemamarketing.dtos.contato.entrada;
+
+public class ProdutoDTO {
+}
