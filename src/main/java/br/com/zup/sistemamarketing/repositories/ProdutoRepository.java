62e34d8e92a3c347a31f43394efc396fc1ba2f6b
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-21 00:26:19 -0300

    Criar método para buscar produto pelo nome

--- src/main/java/br/com/zup/sistemamarketing/repositories/ProdutoRepository.java
+++ src/main/java/br/com/zup/sistemamarketing/repositories/ProdutoRepository.java
@@ -3,5 +3,8 @@ package br.com.zup.sistemamarketing.repositories;
 import br.com.zup.sistemamarketing.models.Produto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+    Optional<Produto> findByNome(String nome);
 }
