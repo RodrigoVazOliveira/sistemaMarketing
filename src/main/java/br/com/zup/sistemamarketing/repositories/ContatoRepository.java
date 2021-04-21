50f06acda5b9652f8c4f7bf621ac5b5b58cd9f42
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-21 08:53:50 -0300

    Criar método de consulta caso exista o contato por email

--- src/main/java/br/com/zup/sistemamarketing/repositories/ContatoRepository.java
+++ src/main/java/br/com/zup/sistemamarketing/repositories/ContatoRepository.java
@@ -3,5 +3,8 @@ package br.com.zup.sistemamarketing.repositories;
 import br.com.zup.sistemamarketing.models.Contato;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ContatoRepository extends CrudRepository<Contato, Integer> {
+    Boolean existsByEmail(String email);
 }
