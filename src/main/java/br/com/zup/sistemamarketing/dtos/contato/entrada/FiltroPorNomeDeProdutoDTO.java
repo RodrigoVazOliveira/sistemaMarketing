c9e5496a303062face93eb33a535fec1c5ad738a
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-22 08:48:09 -0300

    Criar dto para filtro de nome de produto

---
+++ src/main/java/br/com/zup/sistemamarketing/dtos/contato/entrada/FiltroPorNomeDeProdutoDTO.java
@@ -0,0 +1,16 @@
+package br.com.zup.sistemamarketing.dtos.contato.entrada;
+
+public class FiltroPorNomeDeProdutoDTO {
+    private String nome;
+
+    public FiltroPorNomeDeProdutoDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
