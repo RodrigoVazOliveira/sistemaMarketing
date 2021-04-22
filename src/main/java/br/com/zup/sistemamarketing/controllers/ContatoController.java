4a56132beda94eeb1aeaed20b2c4020651084937
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-22 16:38:11 -0300

    Retirar tratametno de exceção no controller

--- src/main/java/br/com/zup/sistemamarketing/controllers/ContatoController.java
+++ src/main/java/br/com/zup/sistemamarketing/controllers/ContatoController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -30,14 +29,10 @@ public class ContatoController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ContatoDTO cadastrarContato(@RequestBody @Valid CadastrarContatoDTO cadastrarContatoDTO) {
-        try {
-            Contato contato = contatoService.cadastrarNovoContato(
-                    cadastrarContatoDTO.converterDtoParaModelo()
-            );
-            return ContatoDTO.converterModeloParaDto(contato);
-        } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+        Contato contato = contatoService.cadastrarNovoContato(
+                cadastrarContatoDTO.converterDtoParaModelo()
+        );
+        return ContatoDTO.converterModeloParaDto(contato);
     }
 
     @GetMapping
@@ -52,14 +47,10 @@ public class ContatoController {
     public ResponseEntity<ContatoDTO> atualizarContatoCompleto(@PathVariable Integer id,
                                                                @RequestBody @Valid
                                                                        AtualizarContatoDTO atualizarContatoDTO) {
-        try {
-            if (contatoService.contatoExiste(id)) {
-              return atualizarContatoExistente(atualizarContatoDTO.converterDtoParaModelo(id));
-            }
-            return atualizarContatoNaoExistente(atualizarContatoDTO.converterDtoParaModelo(null));
-        } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        if (contatoService.contatoExiste(id)) {
+          return atualizarContatoExistente(atualizarContatoDTO.converterDtoParaModelo(id));
         }
+        return atualizarContatoNaoExistente(atualizarContatoDTO.converterDtoParaModelo(null));
     }
 
     private ResponseEntity<ContatoDTO> atualizarContatoExistente(Contato contato) {
@@ -80,25 +71,17 @@ public class ContatoController {
                                                    @Valid
                                                    AtualizarParcialContatoDTO atualizarParcialContatoDTO) {
 
-            try {
-                Contato contatoAtualizado = contatoService.atualizarContatoParcialmente(
-                        atualizarParcialContatoDTO.converterDtoParaModelo(id)
-                );
-                return ContatoDTO.converterModeloParaDto(contatoAtualizado);
-            } catch (RuntimeException e) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-            }
+            Contato contatoAtualizado = contatoService.atualizarContatoParcialmente(
+                    atualizarParcialContatoDTO.converterDtoParaModelo(id)
+            );
+            return ContatoDTO.converterModeloParaDto(contatoAtualizado);
 
     }
 
     @DeleteMapping("{id}/")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void excluirContatoPorId(@PathVariable Integer id) {
-        try {
-            contatoService.excluirContatoPorId(id);
-        } catch (RuntimeException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        }
+        contatoService.excluirContatoPorId(id);
     }
 
     @GetMapping("produtos")
