b8e40f5cb35a6d21d1327cb748ef9fd4133e29b5
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-22 11:25:43 -0300

    Criar objeto de erro dos campos

---
+++ src/main/java/br/com/zup/sistemamarketing/exceptions/validacao/ErroDeValidacao.java
@@ -0,0 +1,31 @@
+package br.com.zup.sistemamarketing.exceptions.validacao;
+
+public class ErroDeValidacao {
+
+    private String campo;
+    private String mensagem;
+
+    public ErroDeValidacao() {
+    }
+
+    public ErroDeValidacao(String campo, String mensagem) {
+        this.campo = campo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
