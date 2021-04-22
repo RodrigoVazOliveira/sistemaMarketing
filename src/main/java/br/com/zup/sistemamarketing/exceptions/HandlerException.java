ca934e70be6f48545368468e367a6104750d3621
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-22 11:31:35 -0300

    Criar método para gerar lista de erros dos campos

--- src/main/java/br/com/zup/sistemamarketing/exceptions/HandlerException.java
+++ src/main/java/br/com/zup/sistemamarketing/exceptions/HandlerException.java
@@ -1,12 +1,17 @@
 package br.com.zup.sistemamarketing.exceptions;
 
+import br.com.zup.sistemamarketing.exceptions.validacao.ErroDeValidacao;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class HandlerException extends ResponseEntityExceptionHandler {
@@ -14,5 +19,14 @@ public class HandlerException extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+
+    private List<ErroDeValidacao> gerarListaDeErroDeCampo(MethodArgumentNotValidException e) {
+        List<ErroDeValidacao> erros = new ArrayList<>();
+        for (FieldError fieldError : e.getFieldErrors()) {
+            erros.add(new ErroDeValidacao(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+
+        return erros;
     }
 }
