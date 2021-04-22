13ef0edeaf361cb43fb8eff16529bc9f16465871
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-22 11:23:32 -0300

    Criar o manipulador de exceções

---
+++ src/main/java/br/com/zup/sistemamarketing/exceptions/HandlerException.java
@@ -0,0 +1,8 @@
+package br.com.zup.sistemamarketing.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class HandlerException extends ResponseEntityExceptionHandler {
+}
