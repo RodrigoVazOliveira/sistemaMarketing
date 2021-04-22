f1bb25b8c2d50573b019dc72be6d03fdb36723d4
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-04-22 10:01:27 -0300

    Criar método query para filtrar contato por nome de categoria de produto

--- src/main/java/br/com/zup/sistemamarketing/repositories/ContatoRepository.java
+++ src/main/java/br/com/zup/sistemamarketing/repositories/ContatoRepository.java
@@ -3,9 +3,8 @@ package br.com.zup.sistemamarketing.repositories;
 import br.com.zup.sistemamarketing.models.Contato;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface ContatoRepository extends CrudRepository<Contato, Integer> {
     Boolean existsByEmail(String email);
     Iterable<Contato> findByProdutosNome(String nome);
+    Iterable<Contato> findByProdutosCategoriasNome(String nome);
 }
